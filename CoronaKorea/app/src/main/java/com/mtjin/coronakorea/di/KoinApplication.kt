556cd19840d94b671d52110815a41d845947226a
jackjackE <tmddjs210@naver.com>
2020-04-18 15:54:51 +0900

    KoinApplication 추가

---
+++ CoronaKorea/app/src/main/java/com/mtjin/coronakorea/di/KoinApplication.kt
@@ -0,0 +1,25 @@
+package com.mtjin.coronakorea.di
+
+import android.app.Application
+import com.mtjin.coronakorea.BuildConfig
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class KoinApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            if (BuildConfig.DEBUG) {
+                androidLogger()
+            } else {
+                androidLogger(Level.NONE)
+            }
+            androidContext(this@KoinApplication)
+            modules(
+            )
+
+        }
+    }
+}
