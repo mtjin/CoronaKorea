c43bcc3d56051a6c1816332986886d5319d004e6
jackjackE <tmddjs210@naver.com>
2020-04-26 22:04:23 +0900

    apiModule 추가

--- CoronaKorea/app/src/main/java/com/mtjin/coronakorea/api/ApiClient.kt
+++ CoronaKorea/app/src/main/java/com/mtjin/coronakorea/api/ApiClient.kt
@@ -1,15 +1,6 @@
 package com.mtjin.coronakorea.api
 
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
 object ApiClient {
-    private const val BASE_URL = "http://api.corona-19.kr/"
-    fun getApiClient(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
+    const val BASE_URL = "http://api.corona-19.kr/"
 
 }
---
+++ CoronaKorea/app/src/main/java/com/mtjin/coronakorea/module/ApiModule.kt
@@ -0,0 +1,16 @@
+package com.mtjin.coronakorea.module
+
+import com.mtjin.coronakorea.api.ApiClient
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val apiModule: Module = module {
+    single<Retrofit> {
+        Retrofit.Builder()
+            .baseUrl(ApiClient.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
