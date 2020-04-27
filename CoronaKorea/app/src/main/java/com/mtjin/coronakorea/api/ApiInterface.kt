8f873f2d425318eba771013b886eb80f1c07d56e
jackjackE <tmddjs210@naver.com>
2020-04-27 15:55:13 +0900

    ApiInterface 함수명 변경

--- CoronaKorea/app/src/main/java/com/mtjin/coronakorea/api/ApiInterface.kt
+++ CoronaKorea/app/src/main/java/com/mtjin/coronakorea/api/ApiInterface.kt
@@ -9,12 +9,12 @@ import retrofit2.http.Query
 interface ApiInterface {
 
     @GET("korea/country/new/")
-    fun getCountriesInfo(
+    fun getCityInfo(
         @Query("serviceKey") serviceKey: String = "3808684a386e1a1b7e0e5faf9c5a4a223"
     ): Call<CityResponse>
 
     @GET("korea/")
-    fun getTotalCountInfo(
+    fun getCountryInfo(
         @Query("serviceKey") serviceKey: String = "3808684a386e1a1b7e0e5faf9c5a4a223"
     ): Call<CountryResponse>
 }
