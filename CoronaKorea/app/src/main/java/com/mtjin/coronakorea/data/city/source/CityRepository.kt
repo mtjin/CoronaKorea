71d7bdab94f8dd9ef77b95da2862a1ed56624793
jackjackE <tmddjs210@naver.com>
2020-04-28 23:01:22 +0900

    City Repository 구현 완료

--- CoronaKorea/app/src/main/java/com/mtjin/coronakorea/data/city/source/CityRepository.kt
+++ CoronaKorea/app/src/main/java/com/mtjin/coronakorea/data/city/source/CityRepository.kt
@@ -1,4 +1,16 @@
 package com.mtjin.coronakorea.data.city.source
 
+import com.mtjin.coronakorea.data.city.CityResponse
+import com.mtjin.coronakorea.data.city.Korea
+
 interface CityRepository {
+    fun getSearchCity(
+        success: (CityResponse) -> Unit,
+        fail: (Throwable) -> Unit
+    )
+
+    fun getSearchKorea(
+        success: (Korea) -> Unit,
+        fail: (Throwable) -> Unit
+    )
 }
--- CoronaKorea/app/src/main/java/com/mtjin/coronakorea/data/city/source/CityRepositoryImpl.kt
+++ CoronaKorea/app/src/main/java/com/mtjin/coronakorea/data/city/source/CityRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.mtjin.coronakorea.data.city.source
 
+import com.mtjin.coronakorea.data.city.CityResponse
+import com.mtjin.coronakorea.data.city.Korea
 import com.mtjin.coronakorea.data.city.source.local.CityLocalDataSource
 import com.mtjin.coronakorea.data.city.source.remote.CityRemoteDataSource
 import com.mtjin.coronakorea.utils.NetworkManager
@@ -8,4 +10,38 @@ class CityRepositoryImpl(
     private val cityRemoteDataSource: CityRemoteDataSource,
     private val cityLocalDataSource: CityLocalDataSource,
     private val networkManager: NetworkManager
-) : CityRepository
+) : CityRepository {
+    override fun getSearchCity(success: (CityResponse) -> Unit, fail: (Throwable) -> Unit) {
+        if (networkManager.checkNetworkState()) {
+            cityRemoteDataSource.getCity(
+                success = {
+                    success(it)
+                },
+                fail = {
+                    fail(Throwable())
+                }
+            )
+        } else {
+            fail(Throwable())
+        }
+    }
+
+    override fun getSearchKorea(success: (Korea) -> Unit, fail: (Throwable) -> Unit) {
+        if (networkManager.checkNetworkState()) {
+            //캐싱은 애니메이션 효과때문에 없는게 나은듯
+            //remote
+            cityRemoteDataSource.getCity(
+                success = {
+                    cityLocalDataSource.insertKorea(it.korea)
+                    success(it.korea)
+                },
+                fail = {
+                    fail(Throwable())
+                }
+            )
+        } else {
+            //local
+            cityLocalDataSource.getKorea()?.let { success(it) }
+        }
+    }
+}
