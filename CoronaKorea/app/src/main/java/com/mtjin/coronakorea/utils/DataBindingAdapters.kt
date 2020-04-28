0400de97ba47d0cbaeb1ca47bc152211b4651e0a
jackjackE <tmddjs210@naver.com>
2020-04-28 13:59:12 +0900

    비율 관련 로직 오류 수정

--- CoronaKorea/app/src/main/java/com/mtjin/coronakorea/utils/DataBindingAdapters.kt
+++ CoronaKorea/app/src/main/java/com/mtjin/coronakorea/utils/DataBindingAdapters.kt
@@ -1,6 +1,7 @@
 package com.mtjin.coronakorea.utils
 
 import android.graphics.Color
+import android.util.Log
 import androidx.databinding.BindingAdapter
 import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.charts.PieChart
@@ -9,6 +10,7 @@ import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.utils.ColorTemplate
+import com.mtjin.coronakorea.R
 import com.mtjin.coronakorea.data.country.CountryResponse
 
 @BindingAdapter("setTop5Values")
@@ -28,8 +30,25 @@ fun PieChart.setTop5ValuesChart(countryResponse: CountryResponse?) {
             add(PieEntry(it.city3p.toFloat(), it.city3n))
             add(PieEntry(it.city4p.toFloat(), it.city4n))
             add(PieEntry(it.city5p.toFloat(), it.city5n))
-        }
+            Log.d("BBB", "" + it.city1p.toFloat())
+            Log.d("BBB", "" + it.city2p.toFloat())
+            Log.d("BBB", "" + it.city3p.toFloat())
+            Log.d("BBB", "" + it.city4p.toFloat())
+            Log.d("BBB", "" + it.city5p.toFloat())
+            Log.d("BBB", "" + it.city1n)
+            Log.d("BBB", "" + it.city2n)
+            Log.d("BBB", "" + it.city3n)
+            Log.d("BBB", "" + it.city4n)
+            Log.d("BBB", "" + it.city5n)
+            val remainPercent: Float =
+                100 - it.city1p.toFloat() - it.city2p.toFloat() - it.city3p.toFloat() - it.city4p.toFloat() - it.city5p.toFloat()
+            if (remainPercent.toInt() != 0) {
+                add(
+                    PieEntry(remainPercent, context.getString(R.string.remain_text))
+                )
+            }
 
+        }
         animateY(1000, Easing.EaseInOutCubic)
 
         val dataSet: PieDataSet = PieDataSet(yValues, "")
@@ -45,6 +64,61 @@ fun PieChart.setTop5ValuesChart(countryResponse: CountryResponse?) {
             setValueTextColor(Color.BLACK)
             val des = Description()
             des.text = "도시별 확진자 상위 5곳"
+            description = des
+        }
+        data = pieData
+    }
+}
+
+@BindingAdapter("setCureOrDeadValues")
+fun PieChart.setCureOrDeadValuesChart(countryResponse: CountryResponse?) {
+    countryResponse?.let {
+        setUsePercentValues(true)
+        description.isEnabled = false
+        setExtraOffsets(5f, 10f, 5f, 5f)
+        isDrawHoleEnabled = true
+        setHoleColor(Color.WHITE)
+        transparentCircleRadius = 61f
+
+        val yValues: ArrayList<PieEntry> = ArrayList()
+        with(yValues) {
+            add(
+                PieEntry(
+                    it.recoveredPercentage.toFloat(),
+                    context.getString(R.string.country_cure_rate_text)
+                )
+            )
+            add(
+                PieEntry(
+                    it.deathPercentage.toFloat(),
+                    context.getString(R.string.country_death_rate_text)
+                )
+            )
+            val remainPercent: Float =
+                100 - it.deathPercentage.toFloat() - it.recoveredPercentage.toFloat()
+            add(
+                PieEntry(
+                    remainPercent,
+                    context.getString(R.string.etc_text)
+                )
+            )
+        }
+
+        animateY(1000, Easing.EaseInOutCubic)
+
+        val dataSet: PieDataSet = PieDataSet(yValues, "")
+        with(dataSet) {
+            sliceSpace = 3f
+            selectionShift = 5f
+            setColors(*ColorTemplate.PASTEL_COLORS)
+        }
+
+        val pieData: PieData = PieData(dataSet)
+        with(pieData) {
+            setValueTextSize(10f)
+            setValueTextColor(Color.BLACK)
+            val des = Description()
+            des.text = "국내 완치 및 사망 상대비율"
             description = des
         }
         data = pieData
